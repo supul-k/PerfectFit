fc97bd22e8010fdd19ec7423a43b78c7afcd0f61
Supul Kalhara <supulkalhara7@gmail.com>
2023-07-18 23:38:26 +0530

    blog saving now in firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,21 +1,38 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFMZiI81PLeSUfWSZJA78ppkOuLfgt8zU",
-    authDomain: "perfect-fit-web.firebaseapp.com",
-    projectId: "perfect-fit-web",
-    storageBucket: "perfect-fit-web.appspot.com",
-    messagingSenderId: "89161650010",
-    appId: "1:89161650010:web:fe2058ac70b91f7e5aed63"
+  // Your Firebase project's configuration
+    apiKey: "AIzaSyAsbQQ1v4-iFojDtq_ZMnGQjD05p1NyqR4",
+    authDomain: "perfect-fit-web-6435f.firebaseapp.com",
+    projectId: "perfect-fit-web-6435f",
+    storageBucket: "perfect-fit-web-6435f.appspot.com",
+    messagingSenderId: "281776326236",
+    appId: "1:281776326236:web:19a0d34eeda067c7268899",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firestore = getFirestore(app);
 
-// Export the necessary variables if needed
-export { app, db };  
+const publishButton = document.querySelector('.publish-btn');
+publishButton.addEventListener('click', async () => {
+  const blogTitle = document.querySelector('.title').value;
+  const blogArticle = document.querySelector('.article').value;
 
+  const blogsCollection = collection(firestore, 'blogs');
+  const blogData = {
+    title: blogTitle,
+    article: blogArticle,
+    // Add any other blog details you want to save
+  };
 
-
+  try {
+    const docRef = await addDoc(blogsCollection, blogData);
+    console.log('Blog details saved successfully with ID:', docRef.id);
+    // Redirect to a success page or perform any other desired action
+  } catch (error) {
+    console.error('Error saving blog details:', error);
+    // Handle the error appropriately
+  }
+});
