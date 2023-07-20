26f108396e9c67f142a0a84ceb7612399c5ea4c3
Supul Kalhara <supulkalhara7@gmail.com>
2023-07-20 17:31:01 +0530

    quiz saving in fire

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,10 @@
 /* JavaScript codes of quiz.html file */
 
 /* Question 01... */
+// Attach event listeners to buttons
+document.getElementById('backBtn').addEventListener('click', goBack);
+document.getElementById('nextBtn').addEventListener('click', showQuestion2);
+document.getElementById('nextBtn2').addEventListener('click', showQuestion2);
 
 // Function to show question2 and hide question1
 function showQuestion2() {
@@ -28,10 +32,6 @@ function goBack() {
     window.location.href = '/Home/home.html';
 }
 
-// Attach event listeners to buttons
-document.getElementById('backBtn').addEventListener('click', goBack);
-document.getElementById('nextBtn').addEventListener('click', showQuestion2);
-
 
 
 /* Question 02... */
@@ -48,7 +48,7 @@ function goToQuestion(questionNumber) {
     document.getElementById("question" + questionNumber).style.display = "block";
 }
 
-function validateAndGoToNextQuestion(nextQuestionNumber) {
+function validateAndGoToNextQuestion() {
     var selectedOvals = document.getElementsByClassName("oval selected");
     if (selectedOvals.length === 0) {
         alert("Please select at least one option.");
@@ -56,7 +56,7 @@ function validateAndGoToNextQuestion(nextQuestionNumber) {
         // Hide current question
         document.getElementById("question2").style.display = "none";
         // Show the next question
-        document.getElementById("question" + nextQuestionNumber).style.display = "block";
+        document.getElementById("question" + 3).style.display = "block";
     }
 }
 
@@ -359,14 +359,14 @@ function startLoadingAnimation() {
             // Show the "next" button and disable loading
             nextButton.style.display = "block";
             nextButton.disabled = false;
-            loadingSection.style.display = "none";
+            // loadingSection.style.display = "none";
         } else {
             progress++;
         }
     }
 
     updateLoadingText();
-    var interval = setInterval(updateLoadingText, 1000);
+    var interval = setInterval(updateLoadingText, 4000);
 
     // Hide the "next" button and disable it initially
     nextButton.style.display = "none";
@@ -374,276 +374,3 @@ function startLoadingAnimation() {
     // Show the loading section
     loadingSection.style.display = "block";
 }
-
-
-
-
-
-
-
-
-
-/*const loadingText = document.querySelector('.loading-text');
-let percentage = 0;
-let intervalId;
-
-function startLoadingAnimation() {
-  percentage = 0;
-  loadingText.textContent = '0%';
-  intervalId = setInterval(() => {
-    if (percentage === 10) {
-      clearInterval(intervalId);
-      proceedToNextSection();
-    } else {
-      percentage++;
-      loadingText.textContent = `${percentage}%`;
-    }
-  }, 1000);
-}
-
-function proceedToNextSection() {
-  const loadingSection = document.querySelector('#loading');
-  const nextSection = document.querySelector('#next');
-  loadingSection.style.display = 'none';
-  nextSection.style.display = 'block';
-}
-
-// Handle click event on the "Next" button in question11 or question12
-const nextButton = document.querySelector('#next-button');
-nextButton.addEventListener('click', () => {
-  clearInterval(intervalId);
-  startLoadingAnimation();
-});
-
-// Initial loading animation start
-startLoadingAnimation();  */
-
-
-
-
-
-/*function toggleOval(element) {
-    element.classList.toggle("selected");
-}
-
-function showQuestion1() {
-    // Show the first question and hide the second question
-    document.getElementById("question1").style.display = "block";
-    document.getElementById("middle").style.display = "none";
-}
-
-function showmiddle() {
-    // Validate the first question before proceeding
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
-    var height = document.getElementById("height").value;
-
-    if (!name || !age || !height) {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    // Hide the first question and show the second question
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("middle").style.display = "block";
-    document.getElementById("question3").style.display = "none"; // Hide question3
-}
-
-function showQuestion3() {
-    // Validate the second question before proceeding
-    var selectedOvals = document.querySelectorAll(".oval.selected");
-    if (selectedOvals.length === 0) {
-        alert("Please select at least one option.");
-        return;
-    }
-
-    // Hide the second question and show the third question
-    document.getElementById("middle").style.display = "none";
-    document.getElementById("question3").style.display = "block";
-    document.getElementById("question4").style.display = "none";
-}
-
-function showQuestion4() {
-    // Validate the third question before proceeding
-    var bust = document.getElementById("bust").value;
-    var waist = document.getElementById("waist").value;
-    var hip = document.getElementById("hip").value;
-
-    if (!bust || !waist || !hip) {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    // Hide the third question and show the fourth question
-    document.getElementById("question3").style.display = "none";
-    document.getElementById("question4").style.display = "block";
-    document.getElementById("question5").style.display = "none";
-}
-
-function showQuestion5() {
-    // Validate the fourth question before proceeding
-    var dress = document.getElementById("dress").value;
-    var tshirt = document.getElementById("tshirt").value;
-    var pants = document.getElementById("pants").value;
-
-    if (!dress || !tshirt || !pants) {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    // Hide the fourth question and show the fifth question
-    document.getElementById("question4").style.display = "none";
-    document.getElementById("question5").style.display = "block";
-    document.getElementById("question6").style.display = "block";
-}
-
-function showQuestion6() {
-    // Validate the fifth question before proceeding
-    var color = document.getElementById("color").value;
-
-    if (!color) {
-        alert("Please select a color.");
-        return;
-    }
-
-    // Hide the fifth question and show the sixth question
-    document.getElementById("question5").style.display = "none";
-    document.getElementById("question6").style.display = "block";
-}
-
-function showQuestion7() {
-    // Validate the sixth question before proceeding
-    var skinTone = document.getElementById("skin").value;
-    var skinImage = document.getElementById("skin-image").value;
-
-    if (!skinTone || !skinImage) {
-        alert("Please fill in all fields.");
-        return;
-    }
-
-    // Hide the sixth question and show the seventh question
-    document.getElementById("question6").style.display = "none";
-    document.getElementById("question7").style.display = "block";
-}
-
-function showQuestion8() {
-    // Validate the seventh question before proceeding
-    var occasion = document.getElementById("occasion").value;
-
-    if (!occasion) {
-        alert("Please select an occasion.");
-        return;
-    }
-
-    // Hide the seventh question and show the eighth question
-    document.getElementById("question7").style.display = "none";
-    document.getElementById("question8").style.display = "block";
-}
-
-function showQuestion9() {
-    // Validate the eighth question before proceeding
-    var style = document.getElementById("style").value;
-
-    if (!style) {
-        alert("Please select a style.");
-        return;
-    }
-
-    // Hide the eighth question and show the ninth question
-    document.getElementById("question8").style.display = "none";
-    document.getElementById("question9").style.display = "block";
-}
-
-function showQuestion10() {
-    // Validate the ninth question before proceeding
-    var fabric = document.getElementById("fabric").value;
-
-    if (!fabric) {
-        alert("Please select a fabric.");
-        return;
-    }
-
-    // Hide the ninth question and show the tenth question
-    document.getElementById("question9").style.display = "none";
-    document.getElementById("question10").style.display = "block";
-    }
-
-
-
-function showQuestion11() {
-    // Hide the tenth question and show the eleventh question
-    document.getElementById("question10").style.display = "none";
-    document.getElementById("question11").style.display = "block";
-}
-
-
-
-
-
-
-
-// Back button event listeners
-document.getElementById("backBtn").addEventListener("click", function () {
-    window.location.href = "../Home/home.html";
-});
-
-document.getElementById("backBtn2").addEventListener("click", function () {
-    showQuestion1();
-});
-
-document.getElementById("backBtn3").addEventListener("click", function () {
-    showmiddle();
-});
-
-document.getElementById("backBtn4").addEventListener("click", function () {
-    showQuestion3();
-});
-
-document.getElementById("backBtn5").addEventListener("click", function () {
-    showQuestion4();
-});
-
-document.getElementById("backBtn6").addEventListener("click", function () {
-    showQuestion5();
-});
-
-document.getElementById("backBtn7").addEventListener("click", function () {
-    showQuestion6();
-});
-
-document.getElementById("backBtn8").addEventListener("click", function () {
-    showQuestion7();
-});
-
-document.getElementById("backBtn9").addEventListener("click", function () {
-    showQuestion8();
-});
-
-function showQuestion10Back() {
-    // Hide the eleventh question and show the tenth question
-    document.getElementById("question11").style.display = "none";
-    document.getElementById("question10").style.display = "block";
-}
-*/
-
-
-
-  // Next button event listener
-/*document.getElementById("nextBtn").addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-  showQuestion2();
-});
- 
-document.getElementById("nextBtn2").addEventListener("click", function () {
-  showQuestion3();
-});
- 
-document.getElementById("nextBtn3").addEventListener("click", function () {
-  showQuestion4();
-});
- 
-document.getElementById("nextBtn4").addEventListener("click", function () {
-  showQuestion5();
-}); */
-
-
